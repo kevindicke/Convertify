b496c4530cc1c1e0ea5b53d1473fc241ef13d8b6
Kevin Dicke <k.dicke7@gmail.com>
2019-10-16 21:42:45 -0700

    Finished layout need to complete logic

--- Convertify/ContentView.swift
+++ Convertify/ContentView.swift
@@ -9,8 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstUnit = 0
+    @State private var secondUnit = 1
+    @State private var temp = "0"
+    
+    
+    let volumeUnits = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    var convertified : String {
+        var results = ""
+        if firstUnit == secondUnit {
+            results = temp
+        } else if true {
+            
+        }
+        return results
+    }
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView{
+            Form{
+                Section(header: Text("Temperture to convertify")){
+                     TextField("Temperture", text: $temp)
+                        .keyboardType(.decimalPad)
+                }
+                Section(header: Text("From")){
+                    Picker(volumeUnits[firstUnit], selection: $firstUnit) {
+                        ForEach(0 ..< volumeUnits.count){
+                            Text("\(self.volumeUnits[$0].capitalized)")
+                        }
+                    }
+                .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("To")){
+                     Picker(volumeUnits[firstUnit], selection: $secondUnit) {
+                           ForEach(0 ..< volumeUnits.count){
+                               Text("\(self.volumeUnits[$0].capitalized)")
+                           }
+                       }
+                   .pickerStyle(SegmentedPickerStyle())
+                }
+                Text("\(convertified)°")
+                
+            }
+            .navigationBarTitle("Convertify", displayMode: .inline)
+        }
     }
 }
 
